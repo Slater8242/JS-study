17e821b8c4e710c4c9d4f8dfe847310591b38b15
Riad <riad-esrefov@mail.ru>
2021-04-07 22:39:09 +0300

    Circumctances and cycles added

--- script.js
+++ script.js
@@ -104,27 +104,106 @@
     
 // console.log(isChecked && isClose);
 
-// Задание : Практика 1
-const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели","");  //Всегда в виде числа
+// Задание : Практика 1 (Раздел 2 урок 7)
+// const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели","");  //Всегда в виде числа
 // const questions= []; //  my version
 // questions [0] = prompt ("Один из последних просмотренных фильмов?","");
 // questions [1] = +prompt ("На сколько оцените его?","");
 // questions [2] = prompt ("Один из последних просмотренных фильмов?","");
 // questions [3] = +prompt ("На сколько оцените его?","");
 
-const personalMovideDb = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovideDb = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt ("Один из последних просмотренных фильмов?",""),
-      b = +prompt ("На сколько оцените его?",""),
-      c = prompt ("Один из последних просмотренных фильмов?",""),
-      d = +prompt ("На сколько оцените его?","");
+// const a = prompt ("Один из последних просмотренных фильмов?",""),
+//       b = +prompt ("На сколько оцените его?",""),
+//       c = prompt ("Один из последних просмотренных фильмов?",""),
+//       d = +prompt ("На сколько оцените его?","");
 
-personalMovideDb.movies[a] = b;
-personalMovideDb.movies[c] = d;
-console.log(personalMovideDb);
+// personalMovideDb.movies[a] = b;
+// personalMovideDb.movies[c] = d;
+// console.log(personalMovideDb);
+
+// Условия (Раздел 2 урок 8)
+
+// if (4==4){
+//     console.log("ok!");
+// }else{
+//     console.log("Error");
+// }
+
+// if (4==9){
+//     console.log("ok!");
+// }else{
+//     console.log("Error");
+// }
+
+// const num = 50;
+
+// if (num<49){
+//     console.log("Error");
+// }else if (num > 100){
+//     console.log("Много");
+// }else{
+//     console.log("ok!");
+// }
+
+// //  Тернарный оператор
+// (num === 50) ? console.log("ok!") : console.log("Error");
+
+// const num = 50;
+
+// switch(num){
+//     case 49:
+//         console.log("Not correct");
+//         break;
+//     case 100:
+//         console.log("Not correct");
+//         break;
+//     case 50:
+//         console.log("Correct");
+//         break;
+//     default:
+//         console.log("Not this time");
+//         break;
+// }
+
+// Циклы (Раздел 2 урок 8)
+
+// let num = 50;
+
+// while (num<=55){
+//     console.log(num);
+//     num++;
+// }
+
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while (num<55);
+
+// for (let i=1; i<8; i++) {
+//     console.log(i);
+// }
+
+// for (let i=1; i<8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+// for (let i=1; i<10; i++) {
+//     if(i==6){
+//         // break; // остановит цикл когда переменная дойдёт до 6
+//         continue; // пропускает шаг не прерывая цикл
+//     }
+//     console.log(i);
+// }
+
+//  Задание : Практика 2 (Раздел 2 урок 9)
+
