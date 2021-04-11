1a3365e2d0125cc13a3014da8bc273f8fd3a20c1
Riad <riad-esrefov@mail.ru>
2021-04-12 00:47:07 +0300

    Task 3. I have succesfully wrote the given task but in different style

--- script.js
+++ script.js
@@ -207,7 +207,136 @@
 
 //  Задание : Практика 2 (Раздел 2 урок 9)
 
-const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели","");  //Всегда в виде числа
+// const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели","");  //Всегда в виде числа
+
+// const personalMovieDb = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// // for (var i = 0; i <2; i++) {
+// let i=0;
+// // while (i<2){
+// do{
+//     const a = prompt ("Один из последних просмотренных фильмов?",""),
+//           b = +prompt ("На сколько оцените его?","");
+//           i++;
+//       personalMovieDb.movies[a] = b;
+//       if(a != null && b != null && a != "" && b != "" && a.length<50){
+//         personalMovieDb[a] = b;
+//         console.log("done");
+//       }else{
+//           console.log("Error");
+//           i--;
+//       }
+// }while (i < 2);
+// // }
+// // }
+// console.log (personalMovieDb);
+
+// if(personalMovieDb.count <=10){
+//     alert("Просмотрено довольно мало фильмов");
+// }else if (10<personalMovieDb.count && personalMovieDb.count <= 30) {
+//     alert("Вы классический зритель");
+// }else if (personalMovieDb.count > 30) {
+//     alert("Вы киноман");
+// }else{
+//     alert("Произошло ошибка");
+// }
+
+// Функции, стрелочные функции (Раздел 2 урок 10)
+// function showFirstMessage(){
+//     console.log("Hello world!");
+// }
+
+// showFirstMessage();
+
+// let num = 20; //Глобальная переменная
+
+// function showFirstMessage(text){
+//     console.log(text);
+//     // let num = 20; // Пременная внутри функции локальная переменная
+//     let num = 50;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello world!");
+// console.log(num);
+
+// function calc (a,b){
+//     return (a+b);
+//     // console.log("LOLOL"); // Код после return "мёртвый" потому что код на return заканчивается
+//     // Функия может быть использована, до или после объявления
+// }
+
+// console.log(calc(4,5));
+// console.log(calc(4,11));
+// console.log(calc(21,5));
+
+// function ret(){
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum= ret();
+// console.log(anotherNum);
+
+// const logger = function (){
+//     console.log("Hello");
+//     //  Function Express создаётся только тогда когда доходит поток кода , можно вызвать только после объявления
+// };
+
+// logger();
+
+// const calc = (a,b) => a+b;
+
+// console.log(calc (5,6));
+
+// Методы и свойства строк и чисел (Раздел 2 урок 11)
+
+// const str = "tESt";
+// const arr = [1,2,4]
+
+// console.log(arr.length);
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit")); // Показывает в каком месте находится строка или число
+// console.log(fruit.indexOf("q")); // Если нет такой строки или числа то вернётся -1
+
+// const logg = "hello world!";
+
+// console.log(logg.slice(6,11)); // вырезает слово из строки, нужно указать начальный и конечный индекс
+// console.log(logg.substring(6,11)); 
+// console.log(logg.substr(6,4)); // первый аргумент показывает начало , второй аргумент показываем сколько нужно вырезать
+
+// const num = 12.2;
+
+// console.log(Math.round(num)); //округление
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+//  Задание : Практика 2 (Раздел 2 урок 12)
+
+let numberOfFilms ;
+function start() {
+    numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели",""); 
+
+    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели","");
+    }
+}
+
+start();
 
 const personalMovieDb = {
     count: numberOfFilms,
@@ -218,31 +347,84 @@ const personalMovieDb = {
 };
 
 // for (var i = 0; i <2; i++) {
-let i=0;
-// while (i<2){
-do{
-    const a = prompt ("Один из последних просмотренных фильмов?",""),
-          b = +prompt ("На сколько оцените его?","");
-          i++;
-      personalMovieDb.movies[a] = b;
-      if(a != null && b != null && a != "" && b != "" && a.length<50){
-        personalMovieDb[a] = b;
-        console.log("done");
-      }else{
-          console.log("Error");
-          i--;
-      }
-}while (i < 2);
+// // let i=0;
+// // while (i<2){
+// // do{
+//     const a = prompt ("Один из последних просмотренных фильмов?",""),
+//           b = +prompt ("На сколько оцените его?","");
+//           i++;
+//       personalMovieDb.movies[a] = b;
+//       if(a != null && b != null && a != "" && b != "" && a.length<50){
+//         personalMovieDb[a] = b;
+//         console.log("done");
+//       }else{
+//           console.log("Error");
+//           i--;
+//       }
+// // }while (i < 2);
+// // }
 // }
-// }
-console.log (personalMovieDb);
 
-if(personalMovieDb.count <=10){
-    alert("Просмотрено довольно мало фильмов");
-}else if (10<personalMovieDb.count && personalMovieDb.count <= 30) {
-    alert("Вы классический зритель");
-}else if (personalMovieDb.count > 30) {
-    alert("Вы киноман");
-}else{
-    alert("Произошло ошибка");
+function rememberMyFilms(){
+    for (var i = 0; i <2; i++) {
+        const a = prompt ("Один из последних просмотренных фильмов?",""),
+              b = +prompt ("На сколько оцените его?","");
+              i++;
+            personalMovieDb.movies[a] = b;
+        if(a != null && b != null && a != "" && b != "" && a.length<50){
+            personalMovieDb.movies[a] = b;
+            console.log("done");
+        }else{
+            console.log("Error");
+            i--;
+        }
+    }
 }
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(personalMovieDb.count <=10){
+        alert("Просмотрено довольно мало фильмов");
+    }else if (10<personalMovieDb.count && personalMovieDb.count <= 30) {
+        alert("Вы классический зритель");
+    }else if (personalMovieDb.count > 30) {
+        alert("Вы киноман");
+    }else{
+        alert("Произошло ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function writeYourGenres() { // Моя версия
+    let genreNum = 1;
+    for(var i = 0; i <3; i++){
+        const genresQuestion = prompt(`Ваш любимый жанр под номером ${genreNum++}`)
+        personalMovieDb.genres[genreNum] = genresQuestion;
+    }
+}
+
+writeYourGenres();
+
+function writeYourGenres() { // Версия по уроку
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
+
+function showMyDb() { // Моя версия
+    if (personalMovieDb.privat == false) {
+        console.log (personalMovieDb);
+    }
+}
+
+showMyDb();
+
+function showMyDB (hidden) { // Версия по уроку
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
